d2b41b6cbe2b83422f8f31b6ecc6e1342bb4de95
Subed <subedshahxi@gmail.com>
2025-07-25 16:26:05 +0545

    added get post route as well

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,16 @@ const PORT = 4000;
 app.get("/", (_req: Request, res) => {
   res.send("Hello from TypeScript + Node.js! hello");
 });
+app.get("/products", (_req: Request, res) => {
+  res.send("all the product");
+});
+app.get("/products/:id", (_req: Request, res) => {
+  console.log(_req.params);
+  res.send("A Product");
+});
+app.post("/products", (_req: Request, res) => {
+  res.send("A Product");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
