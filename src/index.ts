c26cedb583549aa4dd74cd568f50279326f7bea8
Subed <subedshahxi@gmail.com>
2025-07-25 17:15:46 +0545

    feat: product router setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Router } from "express";
+import express, { Request } from "express";
+import productRoutes from "./routes/products";
 
 const app = express();
 const PORT = 4000;
@@ -7,22 +8,7 @@ app.get("/", (_req: Request, res) => {
   res.send("Hello from TypeScript + Node.js! hello");
 });
 
-//products endpoints
-
-const router = Router();
-
-router.get("/", (_req: Request, res) => {
-  res.send("all the product");
-});
-router.get("/:id", (_req: Request, res) => {
-  console.log(_req.params);
-  res.send("A Product");
-});
-router.post("/", (_req: Request, res) => {
-  res.send("New Product created");
-});
-
-app.use("/products", router);
+app.use("/products", productRoutes);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -1,1 +1,16 @@
-import { Router } from "express";
+import { Router, Request } from "express";
+//products endpoints
+
+const router = Router();
+
+router.get("/", (_req: Request, res) => {
+  res.send("The List of Product");
+});
+router.get("/:id", (_req: Request, res) => {
+  res.send("A Product");
+});
+router.post("/", (_req: Request, res) => {
+  res.send("New Product created");
+});
+
+export default router;
