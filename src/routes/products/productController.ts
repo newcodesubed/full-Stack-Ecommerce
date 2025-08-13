0106ab1801442852319a32add37cb383073acaa9
newcodesubed <subedshahxi@gmail.com>
2025-08-13 23:33:03 +0545

    feat: getproduct by Id connection with data base

--- src/routes/products/productController.ts
+++ src/routes/products/productController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { db } from "../../db/index";
 import { productsTable } from "../../db/productSchema";
+import { eq } from "drizzle-orm";
 
 export async function listProduct(_req: Request, res: Response) {
   try {
@@ -11,9 +12,16 @@ export async function listProduct(_req: Request, res: Response) {
     res.status(500).send({ error: "Failed to list products" });
   }
 }
-export function getProductById(_req: Request, res: Response) {
+export async function getProductById(_req: Request, res: Response) {
   try {
-    
+    const {id} = _req.params;
+    const [product]= await db.select().from(productsTable).where(eq(productsTable.id, Number(id)));
+    if(!product) {
+      return res.status(404).send({ error: "Product not found" });
+    }else{
+      res.status(200).json(product);
+    }
+  
   } catch (error) {
     console.error("Error getting products:", error);
     res.status(500).send({ error: "Failed to get products" });
