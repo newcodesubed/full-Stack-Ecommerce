7952e82e900f612c80a986508daaa230c6e34cd5
newcodesubed <subedshahxi@gmail.com>
2025-08-14 11:49:50 +0545

    feat: added validating data in creating product

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -6,13 +6,22 @@ import {
   updateProduct,
   deleteProduct,
 } from "./productController";
+import { validateData } from "../../middlerwares/validationMiddleware";
+
+import {z} from 'zod';
+
+const createProductSchema = z.object({
+  name:z.string(),
+  price:z.number({message:"Price must be a number"}),
+});
+
 //products endpoints
 
 const router = Router();
 
 router.get("/", listProduct);
 router.get("/:id", getProductById);
-router.post("/", createProduct);
+router.post("/",validateData(createProductSchema) ,createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
 
